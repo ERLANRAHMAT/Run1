ad2293fff12be24490c0bc29ebd5a523da5b76e3
ERLANRAHMAT <erlanrahmatdwisaputra@gmail.com>
2022-02-26 14:33:00 +0700

    Run1

---
+++ shop/donasi.js
@@ -0,0 +1,29 @@
+const donasibot = () => {
+return `DONASI BISA VIA BANK / E-MONEY
+
+*🏧 BANK*
+
+1. BRI 
+	ㅁ -
+2. BCA 
+	ㅁ -
+3. BNI 
+	ㅁ -
+4. PERMATA
+	ㅁ -
+	
+*💰 E-MONEY*
+
+1. GOPAY
+	ㅁ qris ke owner
+2. OVO
+	ㅁ beku
+3. DANA
+	ㅁ 0858 9269 6324
+	
+Semuanya atas nama AG*****RE
+`
+	}
+
+exports.donasibot = donasibot
+ 
